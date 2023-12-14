e859277eab442339dc2ca2e48dc5557fb8ff1e77
Balamathias <balamathias05@gmail.com>
2023-12-14 22:44:58 +0100

    I just added cool rotate left rotate right Q and pack functions to the CM_ROTR_STACK_Q dot C.

---
+++ cm_rotl_rotr_stack_q.c
@@ -0,0 +1,80 @@
+#include "monty.h"
+
+void cm_rotl(stack_t **stack, unsigned int line_number);
+void cm_rotr(stack_t **stack, unsigned int line_number);
+void cm_stack(stack_t **stack, unsigned int line_number);
+void cm_queue(stack_t **stack, unsigned int line_number);
+
+/**
+ * cm_rotl - Spins the top value of a stack_t linked list to the bottom You could call it rotate-left.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void cm_rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	top->next->prev = *stack;
+	(*stack)->next = top->next;
+	bottom->next = top;
+	top->next = NULL;
+	top->prev = bottom;
+
+	(void)line_number;
+}
+
+/**
+ * cm_rotr - Rotates the bottom value of a stack_t linked list to the top.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void cm_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
+	(void)line_number;
+}
+
+/**
+ * cm_stack - Converts a queue to a stack. You know the drill it's logically easyand you can wrap yourself around ityou know Sam stock is a functionthat converts a stack to a queueand it's as simple as that...
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void cm_stack(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = STACK;
+	(void)line_number;
+}
+
+/**
+ * cm_queue - Converts a stack to a queue.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void cm_queue(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = QUEUE;
+	(void)line_number;
+}
